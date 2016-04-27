363a07ca915180057fd648cbffd652ea8bb293e3
Changshuo Gao <cgao@healthrecoverysolutions.com>
2016-04-27 16:21:17 -0400

    ready to add curl request

---
+++ build.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+#this script is used to initial the incron job
+
+SCRIPT_DIR=/root/script/
+CONFIG_LOG=./config_log
+LOG_INFO=info.sh
+TEMP_INCRONTAB=incron
+CONFIG_LOG_NUM=3
+function copyIncrontab(){
+	incrontab -l > ${TEMP_INCRONTAB}
+}
+
+function copyScript(){
+	$(which mkdir) -p $SCRIPT_DIR 
+	$(which cp) $LOG_INFO $SCRIPT_DIR
+}
+
+function addIntoIncrontab(){
+	c_name="$1"
+	c_dir="$2"
+	c_file="$3"
+	t_dir='$@'
+	t_file='$#'
+	command="${c_dir} IN_MODIFY $(which bash) ${SCRIPT_DIR}${LOG_INFO} ${c_name} ${c_dir} ${c_file} ${t_dir} ${t_file}"
+	echo "${command}" >> ${TEMP_INCRONTAB}
+}
+function loadIncrontab(){
+	incrontab ${TEMP_INCRONTAB}
+	rm -rf ${TEMP_INCRONTAB}
+}
+
+
+# Ensure we are root
+
+if [ ! "`whoami`" = "root" ]; then
+
+
+	echo "Error: You must be root to run this script!";
+
+
+	exit 1;
+
+
+fi
+
+#Ensure we have incrontab
+
+if [ "`which incrontab`" = "" ]; then
+
+	echo "Error: You have to install incron to run this script!"
+
+	exit 1;
+
+fi
+
+
+copyIncrontab
+copyScript
+while read LINE
+do
+	#variable
+	name=""
+	file=""
+	
+	if [[ ! $LINE == \#* ]]; then 
+		IFS=' ' read -a CONFIG_ARRAY <<< "$LINE"
+		if [ ${#CONFIG_ARRAY[@]} == $CONFIG_LOG_NUM ]; then
+
+			name=${CONFIG_ARRAY[0]}
+			#echo $name
+			dir=${CONFIG_ARRAY[1]}
+			#echo $file
+			file=${CONFIG_ARRAY[2]}
+			addIntoIncrontab "$name" "$dir" "$file"
+		fi
+
+	fi
+done < $CONFIG_LOG
+loadIncrontab
---
+++ info.sh
@@ -0,0 +1,98 @@
+#!/bin/bash
+#This script will be triggered by incron and is used to parse the log file and send backup monitroing information to database
+NAME=$1
+DIR=$2
+FILE=$3
+TRIGGER_DIR=$4
+TRIGGER_FILE=$5
+
+START_TIME=""
+END_TIME=""
+ELAPSED_TIME=""
+ERRORS=""
+INTERVAL=""
+
+
+START_TIME_LINE_NUM=7
+END_TIME_LINE_NUM=7
+ELAPSED_TIME_LINE_NUM=4
+ERRORS_LINE_NUM=2
+INTERVAL_LINE_NUM=2
+
+
+if [ "`$(which diff) ${DIR}/${FILE} ${TRIGGER_DIR}/${TRIGGER_FILE}`" = "" ]; then
+	while read LINE
+	do
+		#echo $LINE >> /root/file
+		#parse metrics
+		#echo $TRIGGER_FILE >> /root/file
+
+		if [[ $LINE == StartTime* ]]; then
+			IFS=' ' read -a LINE_ARRAY <<< "$LINE"
+			if [ ${#LINE_ARRAY[@]} == $START_TIME_LINE_NUM ]; then
+				sdow=${LINE_ARRAY[2]:1}
+				sm=${LINE_ARRAY[3]}
+				sdom=${LINE_ARRAY[4]}
+				st=${LINE_ARRAY[5]}
+				sy=${LINE_ARRAY[6]}
+				losy=${#sy}
+				sy=${sy:0:losy-1}
+				START_TIME="$sdow $sm $sdom $st $sy"
+				#test
+				echo $START_TIME >> /root/file
+			fi
+		elif [[ $LINE == EndTime* ]]; then
+			IFS=' ' read -a LINE_ARRAY <<< "$LINE"
+			if [ ${#LINE_ARRAY[@]} == $END_TIME_LINE_NUM ]; then
+				edow=${LINE_ARRAY[2]:1}
+                                em=${LINE_ARRAY[3]}
+                                edom=${LINE_ARRAY[4]}
+                                et=${LINE_ARRAY[5]}
+                                ey=${LINE_ARRAY[6]}
+                                loey=${#ey}
+                                ey=${ey:0:loey-1}
+                                END_TIME="$edow $em $edom $et $ey"
+				#test
+				echo $END_TIME >> /root/file
+			fi
+		
+		elif [[ $LINE == ElapsedTime* ]]; then
+			IFS=' ' read -a LINE_ARRAY <<< "$LINE"
+			if [ ${#LINE_ARRAY[@]} == $ELAPSED_TIME_LINE_NUM ]; then
+			
+				elapsed_time=${LINE_ARRAY[2]:1}
+				elapsed_unit=${LINE_ARRAY[3]}
+				loeu=${#elapsed_unit}
+				elapsed_unit=${elapsed_unit:0:loeu-1}
+				ELAPSED_TIME="$elapsed_time $elapsed_unit"
+				#test
+				echo $ELAPSED_TIME >> /root/file
+			fi
+		elif [[ $LINE == Errors* ]]; then
+			IFS=' ' read -a LINE_ARRAY <<< "$LINE"
+			if [ ${#LINE_ARRAY[@]} == $ERRORS_LINE_NUM ]; then
+				ERRORS=${LINE_ARRAY[1]}
+				#test
+				echo $ERRORS >> /root/file
+			fi
+
+		elif [[ $LINE == Interval* ]]; then
+			IFS=' ' read -a LINE_ARRAY <<< "$LINE"
+			if [ ${#LINE_ARRAY[@]} == $INTERVAL_LINE_NUM ]; then
+			
+				INTERVAL=${LINE_ARRAY[1]}
+				#test
+				echo $INTERVAL >> /root/file
+			fi				
+		fi		
+	done < ${TRIGGER_DIR}/${TRIGGER_FILE}
+	if [ $START_TIME != "" ] && [ $END_TIME != "" ] && [ $ELAPSED_TIME != "" ] && [ $ERRORS != "" ]; then
+		#send curl request
+		:
+	fi
+
+else 
+	:	
+fi
+
+
